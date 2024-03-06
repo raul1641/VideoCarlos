90634f84ac3943676c81e9192e08f3b067246cd7
raul <raul@DESKTOP-UM001QC>
2024-03-06 18:27:00 +0100

    CARLOS APRUEBAME :)

---
+++ RMLGITHUB/src/main/java/com/mycompany/rmlgithub/RMLGITHUB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rmlgithub;
+
+/**
+ *
+ * @author raul
+ */
+public class RMLGITHUB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
